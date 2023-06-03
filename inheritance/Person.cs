2105daa171db414abbcdb90b8bd1fc5eb38cb701
fisansyf <fisansyafa812@students.amikom.ac.id>
2023-06-03 23:20:53 +0700

    tugas praktikum ke-7

---
+++ inheritance/Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    public class Person
+    {
+        //property
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        //constructor
+        public Person(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+
+        public Person()
+        {
+
+        }
+
+        //fungsi
+        public void InfoPerson(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+            Console.WriteLine("Nama saya {0}, dan berumur {1} tahun\n", this.Name, this.Age);
+        }
+    }
+    //class turunan
+    public class karyawan : Person
+    {
+        //property
+        public string karyawanID { get; set; }
+        public string subject { get; set; }
+
+        //constructor
+        public karyawan(string karyawanID, string subject)
+        {
+            this.karyawanID = karyawanID;
+            this.subject = subject;
+        }
+
+        public karyawan()
+        {
+
+        }
+
+        //override
+        public void InfoPerson(string karyawanID, string subject)
+        {
+            this.karyawanID = karyawanID;
+            this.subject = subject;
+            Console.WriteLine("saya memiliki ID {0}, dan saya disini sebagai {1}\n", this.karyawanID, this.subject);
+        }
+    }
+}
---
+++ inheritance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.InfoPerson("Syafa", 18);
+
+            Person person2 = new Person();
+            person2.InfoPerson("Homa", 17);
+
+            //inheritance
+            karyawan karyawan1 = new karyawan();
+            karyawan1.InfoPerson("Fisan", 20);
+            karyawan1.InfoPerson("Fisan123", "BOS");
+
+            karyawan karyawan2 = new karyawan();
+            karyawan2.InfoPerson("bambang", 19);
+            karyawan2.InfoPerson("bambang123", "SPB");
+
+            Console.ReadKey();
+        }
+    }
+}
